9e221fd4bcbc93b60e8847989a4fe29976eb2414
mboudroua <mboudroua95@gmail.com>
2019-03-03 15:17:57 +0100

    commit2

--- theMotherfuckingWebsite/my-app/src/app/app.module.ts
+++ theMotherfuckingWebsite/my-app/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {Hello} from './hello/hello.component'; 
+
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Hello
   ],
   imports: [
     BrowserModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
